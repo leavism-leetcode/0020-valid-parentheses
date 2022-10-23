5d97164bfc072db4b6d6c582460392636a2ce3c4
leavism <ghuydang@gmail.com>
2022-10-23 00:23:34 -0700

    Initial commit

---
+++ src/Solution.java
@@ -0,0 +1,14 @@
+public class Solution {
+
+    public static boolean isValid(String s) {
+        boolean result = false;
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        String in = "()";
+
+        System.out.println(isValid(in));
+    }
+}
