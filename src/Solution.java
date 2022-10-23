f0a92797f6253c83307ccbbf033ee4bf76ecec76
leavism <ghuydang@gmail.com>
2022-10-23 01:09:18 -0700

    Working solution

--- src/Solution.java
+++ src/Solution.java
@@ -1,13 +1,31 @@
+import java.util.HashMap;
+import java.util.Stack;
+
 public class Solution {
 
     public static boolean isValid(String s) {
-        boolean result = false;
+        Stack<Character> bracketStack = new Stack<>();
+        HashMap<Character, Character> bracketMap = new HashMap<>();
+        bracketMap.put(')', '(');
+        bracketMap.put('}', '{');
+        bracketMap.put(']', '[');
 
-        return result;
+        for(int index = 0; index < s.length(); index++) {
+            // if string at index is open bracket, push to stack
+            if(bracketMap.containsValue(s.charAt(index))) {
+                bracketStack.push(s.charAt(index));
+                continue;
+            }
+            // if stack is empty or the top of the stack isn't the open bracket for the current close bracket, return false
+            if (bracketStack.empty() || bracketStack.pop() != bracketMap.get(s.charAt(index))) {
+                return false;
+            }
+        }
+        return bracketStack.empty();
     }
 
     public static void main(String[] args) {
-        String in = "()";
+        String in = "]";
 
         System.out.println(isValid(in));
     }
